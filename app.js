60e7949d08726207d686db7535f3a0fc19557df4
mihailinkai <misha.izgareva@gmail.com>
2019-06-16 14:25:02 +0300

    изменяем кол-во товара

--- app.js
+++ app.js
@@ -9,3 +9,27 @@ $('.product__area').html(catalog.map(function (item) {
 
 
 
+
+
+//по клику изменяем кол-во товара 
+
+
+$(document).ready(function () {
+	
+//	по щелчку вниз
+    $('.stepper-arrow.down').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        return false;
+    });
+  
+//	по щелчку вверх
+	  $('.stepper-arrow.up').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        return false;
+    });
+
+});
